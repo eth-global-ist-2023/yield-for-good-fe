c6630a5ec87dab0495db2116474f14a0ed97c528
AndonMitev <mitevandon94@gmail.com>
2023-11-19 01:39:09 +0300

    align nfts

--- components/lists/NftList.tsx
+++ components/lists/NftList.tsx
@@ -10,10 +10,10 @@ export default function NftList() {
   const { nfts } = useGetTokenUris(data as any);
 
   return (
-    <div className='flex flex-col flex-wrap items-center justify-center px-4'>
-      <div className='w-max-[720px] flex flex-col gap-4'>
+    <div className='m-auto flex w-1/2 rounded-xl p-4'>
+      <div className='flex flex-col  gap-4'>
         <Label className='text-xl font-bold'>Your nfts</Label>
-        <div className='w-max-[620px] flex flex-wrap items-center gap-7'>
+        <div className='flex max-w-[300px]  gap-7'>
           {nfts
             ? nfts.map((nft: any, idx: number) => (
                 <NFTCard nft={nft} key={idx} />
