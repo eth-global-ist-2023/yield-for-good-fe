8ed7838c94afc69211b1b69bb2d6024e36193b59
AndonMitev <mitevandon94@gmail.com>
2023-11-18 22:27:05 +0300

    style nfts

--- components/cards/NftCard.tsx
+++ components/cards/NftCard.tsx
@@ -1,11 +1,25 @@
 'use client';
 
 import Image from 'next/image';
+import { Label } from '../ui/label';
 
 export default function NFTCard({ nft }: { nft: any }) {
   return (
-    <div>
-      <Image src={nft?.image} alt='NFT' width={200} height={200} />
+    <div className='h-[300px] rounded-xl border bg-white shadow-xl transition duration-300 hover:scale-105'>
+      <div className='relative h-[200px] w-[200px]'>
+        <Image
+          src={nft?.image}
+          alt='NFT'
+          fill
+          className='rounded-t-xl'
+          objectFit='cover'
+        />
+      </div>
+      <div className='flex w-full items-center justify-center'>
+        <Label className='text-md mt-4 w-[150px] text-center font-bold text-green-400'>
+          {nft.name}
+        </Label>
+      </div>
     </div>
   );
 }
